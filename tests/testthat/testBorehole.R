dcbf7f5a6941beb9493ba06b53b29ecb9a0179d3
Jeffrey Regier <regier@gmail.com>
2014-10-22 10:22:22 -0700

    renamed some variables in the test files

--- tests/testthat/testBorehole.R
+++ tests/testthat/testBorehole.R
@@ -5,16 +5,16 @@ context("the borehole function")
 
 p = 8
 
-borehole <- function(xx)
+borehole <- function(x)
 {
-  rw <- xx[1]
-  r  <- xx[2]
-  Tu <- xx[3]
-  Hu <- xx[4]
-  Tl <- xx[5]
-  Hl <- xx[6]
-  L  <- xx[7]
-  Kw <- xx[8]
+  rw <- x[1]
+  r  <- x[2]
+  Tu <- x[3]
+  Hu <- x[4]
+  Tl <- x[5]
+  Hl <- x[6]
+  L  <- x[7]
+  Kw <- x[8]
   
   frac1 <- 2 * pi * Tu * (Hu-Hl)
   
@@ -33,37 +33,37 @@ hypercube.to.domain = function(ww) {
 }
 
 borehole.hypercube <- function(ww) {
-  xx = hypercube.to.domain(ww)
-  return(borehole(xx))
+  x = hypercube.to.domain(ww)
+  return(borehole(x))
 }
 
-xs = t(matrix(sapply(0:(2^p-1), function(i) int.to.bits(i, p)), nrow=p))
-xs = rbind(xs, rep(0.5, 8))
-xs2 = matrix(0.5, nrow=16, ncol=8)
+X = t(matrix(sapply(0:(2^p-1), function(i) int.to.bits(i, p)), nrow=p))
+X = rbind(X, rep(0.5, 8))
+X2 = matrix(0.5, nrow=16, ncol=8)
 for (i in 1:8) {
-  xs2[i,i] = 0
-  xs2[8 + i, i] = 1
+  X2[i,i] = 0
+  X2[8 + i, i] = 1
 }
-xs = rbind(xs, xs2)
-ys = apply(xs, 1, borehole.hypercube)
+X = rbind(X, X2)
+f.X = apply(X, 1, borehole.hypercube)
 
-K.hat = find.K.hat(xs, ys)
+K.hat = find.K.hat(X, f.X)
 
 test_that("lower bound on computational burden is trivial---0 or less", {
-  expect_equal(max(lower.bound.computational.burden(xs, ys, 0.001, Inf, K.hat), 0), 0)
+  expect_equal(max(lower.bound.computational.burden(X, f.X, 0.001, Inf, K.hat), 0), 0)
 })
 
 test_that("uncertainty is 0 at all corners, when all corners are in X",{ 
-          expect_equal(corners.uncertainty.bound(xs, ys, K.hat), 0.)
+          expect_equal(corners.uncertainty.bound(X, f.X, K.hat), 0.)
 })
 
-max.uncertainty = branch.and.bound.max.uncertainty(xs, ys, K.hat)
+max.uncertainty = branch.and.bound.max.uncertainty(X, f.X, K.hat)
 test_that("bounds on max uncertainty surround the truth", {
-  expect_true(lower.bound.max.uncertainty(xs, ys, K.hat) <= max.uncertainty)
-  expect_true(upper.bound.max.uncertainty(xs, ys, K.hat) >= max.uncertainty)
+  expect_true(lower.bound.max.uncertainty(X, f.X, K.hat) <= max.uncertainty)
+  expect_true(upper.bound.max.uncertainty(X, f.X, K.hat) >= max.uncertainty)
 })
 
-cbs = uncertainty.confidence.bounds(xs, ys, K.hat, n=5000, confidence=.999)
+cbs = uncertainty.confidence.bounds(X, f.X, K.hat, n=5000, confidence=.999)
 test_that("confidence bounds are sensible", {
   expect_true(cbs[1,1] < 124)
   expect_true(124 < cbs[1,2])
--- tests/testthat/testHighDimCone.R
+++ tests/testthat/testHighDimCone.R
@@ -11,36 +11,35 @@ f <- function(x) {
   max(abs(x - z))
 }
 
-xs = rep(0.5, num.dims)
-xs2 = matrix(0.5, nrow=2*num.dims, ncol=num.dims)
+X = matrix(0.5, nrow=2*num.dims, ncol=num.dims)
 for (i in 1:num.dims) {
-  xs2[i,i] = 0
-  xs2[num.dims + i, i] = 1
+  X[i,i] = 0
+  X[num.dims + i, i] = 1
 }
-xs = rbind(xs, xs2)
-ys = apply(xs, 1, f)
+X = rbind(rep(0.5, num.dims), X)
+f.X = apply(X, 1, f)
 
 test_that("empirical Lipschitz constant is correct with various distance metrics", {
-  expect_equal(find.K.hat(xs, ys, Inf), 1)
-  expect_equal(find.K.hat(xs, ys, 2), 1)
-  expect_equal(find.K.hat(xs, ys, 1), 1)
+  expect_equal(find.K.hat(X, f.X, Inf), 1)
+  expect_equal(find.K.hat(X, f.X, 2), 1)
+  expect_equal(find.K.hat(X, f.X, 1), 1)
 })
 
 K.hat = 1
 
 test_that("pointwise uncertainty meets established conditions", {
-  expect_equal(pointwise.uncertainty(xs, ys, K.hat, z), 0)
-  expect_equal(pointwise.uncertainty(xs, ys, K.hat, rep(1, 21)), .25)
-  expect_true(pointwise.uncertainty(xs, ys, K.hat, runif(21)) < .250001)
-  expect_true(pointwise.uncertainty(xs, ys, K.hat, runif(21)) < .250001)
-  expect_true(pointwise.uncertainty(xs, ys, K.hat, runif(21)) < .250001)
-  expect_true(pointwise.uncertainty(xs, ys, K.hat, runif(21)) < .250001)
+  expect_equal(pointwise.uncertainty(X, f.X, K.hat, z), 0)
+  expect_equal(pointwise.uncertainty(X, f.X, K.hat, rep(1, 21)), .25)
+  expect_true(pointwise.uncertainty(X, f.X, K.hat, runif(21)) < .250001)
+  expect_true(pointwise.uncertainty(X, f.X, K.hat, runif(21)) < .250001)
+  expect_true(pointwise.uncertainty(X, f.X, K.hat, runif(21)) < .250001)
+  expect_true(pointwise.uncertainty(X, f.X, K.hat, runif(21)) < .250001)
 })
 
 test_that("bounds on max uncertainty surround the true value", {
-  expect_true(corners.uncertainty.bound(xs, ys, K.hat) < .250001)
-  expect_true(lower.bound.max.uncertainty(xs, ys, K.hat) < .25001)
-  expect_true(upper.bound.max.uncertainty(xs, ys, K.hat) > 0.24999)
-  expect_equal(branch.and.bound.max.uncertainty(xs, ys, K.hat), 0.25)
+  expect_true(corners.uncertainty.bound(X, f.X, K.hat) < .250001)
+  expect_true(lower.bound.max.uncertainty(X, f.X, K.hat) < .25001)
+  expect_true(upper.bound.max.uncertainty(X, f.X, K.hat) > 0.24999)
+  expect_equal(branch.and.bound.max.uncertainty(X, f.X, K.hat), 0.25)
 })
 
